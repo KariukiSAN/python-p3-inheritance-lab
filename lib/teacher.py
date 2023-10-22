df07bdd0ac435a757df9c82a1e6a1522b5efc3c2
KariukiSAN <kariukian1@gmail.com>
2023-10-22 11:34:10 +0300

    R0

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,13 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        if self.knowledge:
+            index = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[index]
+        else:
+            return "I have no knowledge to share."
